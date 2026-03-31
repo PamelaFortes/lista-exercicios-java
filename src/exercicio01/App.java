6727f5569471897d5621cf4da769ddd66c3964c0
Pamela Fortes <pamelaefg@hotmail.com>
2026-03-31 01:50:11 -0400

    exercício02

--- src/exercicio01/App.java
+++ src/exercicio01/App.java
@@ -6,7 +6,7 @@ b) Imprima os dados de cada carro no console. */
 package exercicio01;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Carro carro1 = new Carro();
         carro1.marca = "Subaru";
         carro1.modelo = "impreza";
@@ -26,5 +26,30 @@ public class App {
         System.out.println("Marca: " + carro2.marca);
         System.out.println("Modelo: " + carro2.modelo);
         System.out.println("Ano: " + carro2.ano);
-    } 
+
+        /*
+         * Exercício 2 – Método void vs método com retorno
+         * Na classe Carro:
+         * a) Crie um método void exibir() que imprime marca, modelo e ano formatados.
+         * b) Crie um método int idadeDoCarro() que retorna 2026 - ano.
+         * c) Na Main, chame os dois métodos. Comente no código: qual devolve valor?
+         * Qual só executa uma ação?
+         */
+
+        carro1.exibir();
+        carro2.exibir();
+        /*
+         * O método exibir() é do tipo void, ou seja, NÃO retorna valor. Ele apenas executa uma ação.
+         * -----------------------------------
+         */
+
+        int idadeCarro = carro1.idadeDoCarro();
+        System.out.println(idadeCarro);
+
+        int idadeCarro2 = carro2.idadeDoCarro();
+        System.out.println(idadeCarro2);
+        /*
+         * - O método idadeDoCarro() retorna um valor do tipo int.Esse valor pode ser armazenado em uma variável ou usado diretamente.
+         */
+    }
 }
--- src/exercicio01/Carro.java
+++ src/exercicio01/Carro.java
@@ -3,5 +3,19 @@ package exercicio01;
 public class Carro {
     String marca;
     String modelo;
-    Integer ano;
+    int ano;
+
+    void exibir() { // método void
+        System.out.println("----------------");
+        System.out.println("Carro1:");
+        System.out.println("Marca:" + marca);
+        System.out.println("Modelo:" + modelo);
+        System.out.println("Ano:" + ano);
+    }
+
+    // método de retorno
+    int idadeDoCarro() {
+        int anoAtual = java.time.Year.now().getValue();
+        return anoAtual - ano;
+    }
 }
