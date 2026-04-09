15088d59131ad4aa8a096773c0d94b15229ff61c
Pamela Fortes <pamelaefg@hotmail.com>
2026-04-08 21:33:08 -0400

    exercicio5

--- src/exercicio01/App.java
+++ src/exercicio01/App.java
@@ -5,15 +5,8 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
 
-        Carro carro1 = new Carro();
-        carro1.setMarca("Subaru");
-        carro1.setModelo("Impreza");
-        carro1.setAno(2005);
-
-        Carro carro2 = new Carro();
-        carro2.setMarca("Honda");
-        carro2.setModelo("Civic");
-        carro2.setAno(2010);
+        Carro carro1 = new Carro("Subaru", "Impreza", 2005);
+        Carro carro2 = new Carro("Honda", "Civic", 2010);
 
         System.out.println("Carro 1:");
         System.out.println("Marca: " + carro1.getMarca());
@@ -31,30 +24,20 @@ public class App {
         System.out.println("Idade carro 1: " + carro1.idadeDoCarro());
         System.out.println("Idade carro 2: " + carro2.idadeDoCarro());
 
-        // ArrayList
+        // ArrayList com construtor
         ArrayList<Carro> listaCarros = new ArrayList<>();
 
-        Carro c1 = new Carro();
-        c1.setMarca("Toyota");
-        c1.setModelo("Corolla");
-        c1.setAno(2022);
-
-        Carro c2 = new Carro();
-        c2.setMarca("Honda");
-        c2.setModelo("Civic");
-        c2.setAno(2020);
-
-        Carro c3 = new Carro();
-        c3.setMarca("Hyundai");
-        c3.setModelo("HB20");
-        c3.setAno(2019);
-
-        listaCarros.add(c1);
-        listaCarros.add(c2);
-        listaCarros.add(c3);
+        listaCarros.add(new Carro("Toyota", "Corolla", 2022));
+        listaCarros.add(new Carro("Honda", "Civic", 2020));
+        listaCarros.add(new Carro("Hyundai", "HB20", 2019));
 
         for (Carro c : listaCarros) {
             c.exibir();
+
+            // Vantagem do construtor:
+            // Permite criar o objeto já com todos os dados definidos,
+            // evitando ter que chamar vários setters depois.
+            // Isso deixa o código mais organizado e seguro.
         }
     }
 }
--- src/exercicio01/Carro.java
+++ src/exercicio01/Carro.java
@@ -6,27 +6,24 @@ public class Carro {
     private String modelo;
     private int ano;
 
+    // CONSTRUTOR PARAMETRIZADO
+    public Carro(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.setAno(ano); // usa a validação
+    }
+
+    // CONSTRUTOR VAZIO (mantido se você já estava usando)
+    public Carro() {}
+
     // GETTERS
-    public String getMarca() {
-        return marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public int getAno() {
-        return ano;
-    }
+    public String getMarca() { return marca; }
+    public String getModelo() { return modelo; }
+    public int getAno() { return ano; }
 
     // SETTERS
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
+    public void setMarca(String marca) { this.marca = marca; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
 
     public void setAno(int ano) {
         if (ano < 1886 || ano > 2026) {
