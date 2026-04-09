692d17f6625aa3828a5a9c18683412a5000a8c63
Pamela Fortes <pamelaefg@hotmail.com>
2026-04-08 20:58:28 -0400

    exercicio4

--- src/exercicio01/App.java
+++ src/exercicio01/App.java
@@ -4,62 +4,55 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
+
         Carro carro1 = new Carro();
-        carro1.marca = "Subaru";
-        carro1.modelo = "impreza";
-        carro1.ano = 2005;
+        carro1.setMarca("Subaru");
+        carro1.setModelo("Impreza");
+        carro1.setAno(2005);
 
         Carro carro2 = new Carro();
-        carro2.marca = "Honda";
-        carro2.modelo = "Civic";
-        carro2.ano = 2010;
+        carro2.setMarca("Honda");
+        carro2.setModelo("Civic");
+        carro2.setAno(2010);
 
         System.out.println("Carro 1:");
-        System.out.println("Marca: " + carro1.marca);
-        System.out.println("Modelo: " + carro1.modelo);
-        System.out.println("Ano: " + carro1.ano);
+        System.out.println("Marca: " + carro1.getMarca());
+        System.out.println("Modelo: " + carro1.getModelo());
+        System.out.println("Ano: " + carro1.getAno());
 
         System.out.println("Carro 2:");
-        System.out.println("Marca: " + carro2.marca);
-        System.out.println("Modelo: " + carro2.modelo);
-        System.out.println("Ano: " + carro2.ano);
+        System.out.println("Marca: " + carro2.getMarca());
+        System.out.println("Modelo: " + carro2.getModelo());
+        System.out.println("Ano: " + carro2.getAno());
 
         carro1.exibir();
         carro2.exibir();
 
-        int idadeCarro = carro1.idadeDoCarro();
-        System.out.println(idadeCarro);
+        System.out.println("Idade carro 1: " + carro1.idadeDoCarro());
+        System.out.println("Idade carro 2: " + carro2.idadeDoCarro());
 
-        int idadeCarro2 = carro2.idadeDoCarro();
-        System.out.println(idadeCarro2);
-
-        /*
-         * Na Main, crie um ArrayList<Carro>. Adicione 3 carros na lista. Percorra com
-         * for-each
-         * chamando exibir() em cada um.
-         * 
-         * ArrayList<Tipo> nome = new ArrayList<Tipo>();
-         */
+        // ArrayList
         ArrayList<Carro> listaCarros = new ArrayList<>();
 
         Carro c1 = new Carro();
-        c1.marca = "Toyota";
-        c1.modelo = "Corolla";
-        c1.ano = 2022;
+        c1.setMarca("Toyota");
+        c1.setModelo("Corolla");
+        c1.setAno(2022);
 
         Carro c2 = new Carro();
-        c2.marca = "Honda";
-        c2.modelo = "Civic";
-        c2.ano = 2020;
+        c2.setMarca("Honda");
+        c2.setModelo("Civic");
+        c2.setAno(2020);
 
         Carro c3 = new Carro();
-        c3.marca = "Hyundai";
-        c3.modelo = "HB20";
-        c3.ano = 2019;
+        c3.setMarca("Hyundai");
+        c3.setModelo("HB20");
+        c3.setAno(2019);
 
         listaCarros.add(c1);
         listaCarros.add(c2);
         listaCarros.add(c3);
+
         for (Carro c : listaCarros) {
             c.exibir();
         }
--- src/exercicio01/Carro.java
+++ src/exercicio01/Carro.java
@@ -1,19 +1,49 @@
 package exercicio01;
 
 public class Carro {
-    String marca;
-    String modelo;
-    int ano;
 
-    void exibir() { // método void
-        System.out.println("----------------");
-        System.out.println("Carro1:");
-        System.out.println("Marca:" + marca);
-        System.out.println("Modelo:" + modelo);
-        System.out.println("Ano:" + ano);
+    private String marca;
+    private String modelo;
+    private int ano;
+
+    // GETTERS
+    public String getMarca() {
+        return marca;
     }
 
-    // método de retorno
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    // SETTERS
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setAno(int ano) {
+        if (ano < 1886 || ano > 2026) {
+            System.out.println("Ano inválido!");
+        } else {
+            this.ano = ano;
+        }
+    }
+
+    // MÉTODOS
+    void exibir() {
+        System.out.println("----------------");
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Ano: " + ano);
+    }
+
     int idadeDoCarro() {
         int anoAtual = java.time.Year.now().getValue();
         return anoAtual - ano;
